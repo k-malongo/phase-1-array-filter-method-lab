10575d46753ce04b14f54dda4f0e40fd5b945234
Kelvin <vokemalosh32@gmail.com>
2022-06-11 15:12:34 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution here
+ function findMatching(drivers, driverNames){
+    const check = drivers.filter(driverr=>driverr.toLowerCase()===driverNames.toLowerCase());
+// //    return (drivers.filter(drivers=>drivers.toLowerCase()===driverNames.toLowerCase()))
+
+
+    if(check){
+        return check;
+    }
+
+
+
+ }
+function fuzzyMatch(drivers, driverNames){
+    //return (drivers.filter(drivers=>drivers.toLowerCase().indexOf(driverNames.toLowerCase())===0))
+    const driver= drivers.filter(drivers=>drivers.toLowerCase().indexOf(driverNames.toLowerCase())===0)
+
+    if(driver){
+        return driver;
+    }
+
+}
+function matchName(drivers,driverNames){
+
+    return (drivers.filter(namedfn=>namedfn.name===driverNames ))
+
+}
